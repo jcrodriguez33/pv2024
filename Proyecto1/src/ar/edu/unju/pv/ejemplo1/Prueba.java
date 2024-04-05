ac2204628344a7bddd080949094ca3b3054001bc
jcrodriguez33 <102835665+jcrodriguez33@users.noreply.github.com>
2024-04-05 20:58:13 -0300

    actualizo clase Prueba.java

--- Proyecto1/src/ar/edu/unju/pv/ejemplo1/Prueba.java
+++ Proyecto1/src/ar/edu/unju/pv/ejemplo1/Prueba.java
@@ -6,7 +6,7 @@ public class Prueba {
 
 	public static void main(String[] args) {
 		int anio = 2028;
-		String saludo = "Hola PV";
+		String saludo = "Hola Programacion Visual";
 		System.out.println(saludo + " " + anio);
 
 		if (anio <= 2024) {
