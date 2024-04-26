7dc3d19562a77e62597835954aedb44c074ef054
elena <elena@elena>
2024-04-26 20:53:23 -0300

    Subo ejemplo con enumerados parametrizados

---
+++ Proyecto1/src/ar/edu/unju/pv/ejemplo4/model/Computadora.java
@@ -0,0 +1,41 @@
+package ar.edu.unju.pv.ejemplo4.model;
+
+import ar.edu.unju.pv.ejemplo4.util.Procesador;
+
+public class Computadora {
+	private String disco;
+	private String memoria;
+	private Procesador procesador;
+	
+	public Computadora(String disco, String memoria, Procesador procesador) {
+		super();
+		this.disco = disco;
+		this.memoria = memoria;
+		this.procesador = procesador;
+	}
+	public String getDisco() {
+		return disco;
+	}
+	public void setDisco(String disco) {
+		this.disco = disco;
+	}
+	public String getMemoria() {
+		return memoria;
+	}
+	public void setMemoria(String memoria) {
+		this.memoria = memoria;
+	}
+	public Procesador getProcesador() {
+		return procesador;
+	}
+	public void setProcesador(Procesador procesador) {
+		this.procesador = procesador;
+	}
+	@Override
+	public String toString() {
+		return "Computadora [disco=" + disco + ", memoria=" + memoria + ", procesador=" + procesador.getDescripcion() + "]";
+	}
+	
+	
+	
+}
